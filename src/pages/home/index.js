baae53e421f4e468539c5a6dc4f114abe291a644
valdislav i <vladoss_19995@mail.ru>
2019-02-14 15:30:55 +0300

    clear project

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,21 +1,6 @@
-import Vue from 'vue';
-import AppVue from './App.vue';
-import AppReact from './App.jsx';
-
 import './index.sass';
 import './index.pug';
 
-import './test-scss.scss';
+export default function () {
 
-import tmpPug from './examplate-inline.inline.pug';
-
-export default () => {
-  console.log(tmpPug());
-
-  new Vue({
-    el: '#test-vue',
-    render: createElem => createElem(AppVue),
-  });
-
-  AppReact();
 };
