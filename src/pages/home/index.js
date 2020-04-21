23d485365078125242a3c8baee98cc767ca1142a
vladislavIl <vladoss_19995@mail.ru>
2020-04-21 18:04:16 +0300

    test maket

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,11 @@
-import './index.sass';
+// import 'index.sass';
 import './index.pug';
+import aboutSeo from './aboutSeo'
+import seoServices from './seoServices'
+import seoCards from './seoCards'
 
 export default function () {
-
+  aboutSeo();
+  seoServices();
+  seoCards();
 };
